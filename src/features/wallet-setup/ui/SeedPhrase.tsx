947c9c301ce2bde0cac7b7cc1a0bc02aeeb8bfaf
deux <deux@deuxs-Laptop.local>
2024-10-08 08:58:23 +0300

    feat: add mask to backup private key, refactoring

--- src/features/wallet-setup/ui/SeedPhrase.tsx
+++ src/features/wallet-setup/ui/SeedPhrase.tsx
@@ -1,9 +1,6 @@
-import { EyeOff } from 'lucide-react';
-import { useState } from 'react';
-import { useTranslation } from 'react-i18next';
-
 import { cn } from '@/shared/lib/utils';
 import { PropsWithClassname } from '@/shared/types/react';
+import { Mask } from '@/shared/ui/mask';
 
 type Props = {
   seedPhrase: string[] | null;
@@ -11,18 +8,11 @@ type Props = {
 };
 
 export const SeedPhrase = ({ seedPhrase, onHideMask, className }: PropsWithClassname<Props>) => {
-  const [isShowMask, setIsShowMask] = useState(true);
-
   if (!seedPhrase) return null;
-
-  const handleHideMask = () => {
-    setIsShowMask(false);
-    onHideMask && onHideMask();
-  };
 
   return (
     <div className="relative grow flex flex-col justify-center space-y-3">
-      <div className={cn('relative flex flex-wrap gap-2 justify-center h-fit w-fit', className)}>
+      <div className={cn('relative h-fit w-fit flex flex-wrap gap-2 justify-center rounded-lg', className)}>
         {seedPhrase?.map((word, idx) => (
           <div
             className="text-base flex items-center justify-center rounded-md border dark:border-neutral-700 dark:bg-secondary/20 px-1 h-12 flex-shrink-0 min-w-[30%] max-w-fit whitespace-nowrap"
@@ -31,27 +21,8 @@ export const SeedPhrase = ({ seedPhrase, onHideMask, className }: PropsWithClass
             {idx + 1}. {word}
           </div>
         ))}
-        <Mask isShowMask={isShowMask} onClick={handleHideMask} />
+        <Mask onHideMask={onHideMask} />
       </div>
     </div>
-  );
-};
-
-const Mask = ({ isShowMask, onClick }: { isShowMask: boolean; onClick: () => void }) => {
-  const { t } = useTranslation();
-
-  return (
-    isShowMask && (
-      <div
-        onClick={onClick}
-        className="absolute flex flex-col justify-center items-center top-0 bottom-0 right-0 left-0 rounded-xl border bg-secondary dark:bg-black/95"
-      >
-        <EyeOff className="h-9 w-9" />
-        <div className="mt-2 text-center">
-          <h3 className="text-sm font-medium">{t('wallet.setup.seedPhrase.mask.title')}</h3>
-          <p className="text-[12px] text-muted-foreground">{t('wallet.setup.seedPhrase.mask.description')}</p>
-        </div>
-      </div>
-    )
   );
 };
---
+++ src/shared/ui/mask.tsx
@@ -0,0 +1,29 @@
+import { EyeOff } from 'lucide-react';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const Mask = ({ onHideMask }: { onHideMask?: () => void }) => {
+  const [isShowMask, setIsShowMask] = useState(true);
+  const { t } = useTranslation();
+
+  const handleHideMask = () => {
+    setIsShowMask(false);
+    onHideMask && onHideMask();
+  };
+
+  return (
+    isShowMask && (
+      <div
+        onClick={handleHideMask}
+        className="absolute flex flex-col justify-center items-center top-0 bottom-0 right-0 left-0 border bg-secondary dark:bg-black/95"
+        style={{ borderRadius: 'inherit' }}
+      >
+        <EyeOff className="h-9 w-9" />
+        <div className="mt-2 text-center">
+          <h3 className="text-sm font-medium">{t('shared.mask.title')}</h3>
+          <p className="text-[12px] text-muted-foreground">{t('shared.mask.description')}</p>
+        </div>
+      </div>
+    )
+  );
+};
