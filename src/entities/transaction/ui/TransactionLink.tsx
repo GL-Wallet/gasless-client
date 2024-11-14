8fbacfa7ab27b30c4899220ed4b4e8b2ffcbc2c2
deux <vladprytula.github@gmail.com>
2024-11-14 16:28:36 +0200

    feat: change TransactionLink

--- src/entities/transaction/ui/TransactionLink.tsx
+++ src/entities/transaction/ui/TransactionLink.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight, Share } from 'lucide-react';
+import { Share } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
 import { cn } from '@/shared/lib/utils';
@@ -12,23 +12,22 @@ type Props = {
   transactionLink: string;
 };
 
-export const TransactionLink = ({ tronscanLink, transactionLink, className }: PropsWithClassname<Props>) => {
+export const TransactionLink = ({ transactionLink, className }: PropsWithClassname<Props>) => {
   const utils = useUtils();
   const { t } = useTranslation();
 
   return (
     <div className={cn('w-full flex items-center justify-between space-x-6', className)}>
       <Button
-        onClick={() => utils.openLink(tronscanLink)}
+        onClick={() => utils.shareURL(transactionLink)}
         variant={'outline'}
-        className="w-full space-x-1 dark:border-neutral-700"
+        className="w-full space-x-2 dark:border-neutral-700 text-xl"
       >
         <span>{t('transaction.link')}</span>
-        <ChevronRight className="size-5" />
+        <Share className="size-5" />
       </Button>
 
       <div className="flex items-center space-x-5">
-        <Share className="size-5" onClick={() => utils.shareURL(transactionLink)} />
         <CopyToClipboard size={5} value={transactionLink} />
       </div>
     </div>
