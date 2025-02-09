371128be5ec030c8a4eb6c213736baac525f283b
Vladyslav Prytula <vladprytula.github@gmail.com>
2025-02-09 17:17:25 +0200

    feat: telegram analytics

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import './_styles/globals.css';
 
+import TelegramAnalytics from '@telegram-apps/analytics';
 import { MainLayout } from '@/pages/(layouts)/MainLayout';
 import ErrorBoundary from '@/shared/ui/error-boundary';
 import { Toaster } from '@/shared/ui/toaster';
@@ -8,6 +9,13 @@ import { Configs } from './_configs';
 import { Loaders } from './_loaders';
 import { Providers } from './_providers';
 import { Routes } from './_routes';
+
+console.log(import.meta.env.VITE_TELEGRAM_ANALYTICS_TOKEN)
+
+TelegramAnalytics.init({
+  token: import.meta.env.VITE_TELEGRAM_ANALYTICS_TOKEN,
+  appName: 'gasless_wallet'
+});
 
 export const App = () => {
   return (
